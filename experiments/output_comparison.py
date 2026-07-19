0a7769ec59aef3f777997a6f0665c584d5885304
Burn- <Palesadi10@gmail.com>
2026-07-19 12:23:20 +0330

    Added tokenizer comparison test and updated ignore rules.

--- experiments/output_comparison.py
+++ experiments/output_comparison.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+import json
 from ufal.udpipe import Model, Pipeline, ProcessingError
 from pymystem3 import Mystem
-from src.pipeline_prototype import BASE_DIR
-MODEL_PATH = BASE_DIR / "models" / "russian-syntagrus-ud-2.5-191206.udpipe"
+base_path = Path(__file__).resolve().parent.parent
+model_path = base_path / "models" / "russian-syntagrus-ud-2.5-191206.udpipe"
 
 def main():
     """
@@ -11,11 +12,38 @@ def main():
     between how the two engines tokenize that can crash the main pipeline when it tries to
     merge the two outputs into one csv file.
     :return:
-    Two outputs one after another to visually check for mismatches.
+    Token number for each of them so that we can check for mismatches in tokenization.
     """
     test_text = f"Из-за дождя А.С. Пушкин не пошел в МГУ... Он кто-то, кто говорит по-русски,но делает ошибки?!"
     # udpipe and mystem instantiations
-    model = Model.load(MODEL_PATH)
+    model = Model.load(str(model_path))
     udpipeline = Pipeline(model, "tokenize", Pipeline.DEFAULT, Pipeline.DEFAULT, "conllu")
     error = ProcessingError()
 
+    mystem = Mystem()
+
+    # processing
+    mystem_result = mystem.analyze(test_text)
+    # mystem_sanitized = json.dumps(mystem_result, ensure_ascii=False, indent=4)
+    clean_ms_tokens = []
+    for item in mystem_result:
+        if "analysis" in item and len(item["analysis"]) > 0:
+            clean_ms_tokens.append(item.get("text", ""))
+
+    udpipe_result = udpipeline.process(test_text, error)
+    clean_ud_tokens = []
+    for line in udpipe_result.split("\n"):
+        if line.strip() == '' or line.startswith('#'):
+            continue
+        columns = line.split("\t")
+        clean_ud_tokens.append(columns[1])
+
+    print(f"""
+    UDPipe tokens: {clean_ud_tokens}
+    UDPipe tokens count: {len(clean_ud_tokens)}
+    Mystem tokens: {clean_ms_tokens}
+    Mystem tokens count: {len(clean_ms_tokens)}
+    """)
+
+if __name__ == "__main__":
+    main()
